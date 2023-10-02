0572604fbca51e812dcf2b73bc8c558b399852a1
Anna Panteleeva <apanteleevadev@gmail.com>
2023-10-02 19:02:12 +0300

    feat: edits close popup

--- practice-1/src/scripts/openPopup.js
+++ practice-1/src/scripts/openPopup.js
@@ -5,12 +5,3 @@ btn.addEventListener("click", () => {
   withinBoundaries = true;
   popup.classList.add("opened");
 });
-
-// console.log(popup.classList.contains("opened"));
-// popup.addEventListener("click", (e) => {
-//   console.log(e.composedPath().includes(popup));
-//   const withinBoundaries = e.composedPath().includes(popup);
-//     if (!withinBoundaries) {
-//       popup.style.visibility = "hidden"; // скрываем элемент т к клик был за его пределами
-//     }
-// });
